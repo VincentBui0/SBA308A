41411125afc2098fd1559dd6a323e8861d95fba8
Vincent Bui <vincent.bui657@gmail.com>
2024-04-07 21:05:12 -0400

    Gave event listeners their own file and changed colors

--- app.mjs
+++ app.mjs
@@ -52,31 +52,4 @@ let getMovie = (movieName) => {
   });
 };
 
-// Call the function and handle the promise
-searchBtn.addEventListener("click", () => {
-  let movieName = movieNameRef.value;
-  if (movieName.length <= 0) {
-    result.innerHTML = `<h3 class="msg">Please Enter A Movie Name</h3>`;
-  } else {
-    getMovie(movieName)
-      .then((data) => {
-        console.log('Movie data:', data);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  }
-});
 
-window.addEventListener("load", () => {
-  let movieName = movieNameRef.value;
-  if (movieName.length > 0) {
-    getMovie(movieName)
-      .then((data) => {
-        console.log('Movie data:', data);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-  }
-});
